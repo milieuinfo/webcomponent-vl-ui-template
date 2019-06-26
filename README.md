df8458d59d05b16c0025b3858ca525c07e04f500
Pieter Beckers <pieter.beckers@vlaanderen.be>
2019-06-26 08:32:12 +0200

    UIG-49: template

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # webcomponent-vl-ui-template
 De standaard template voor websites en applicaties van de Vlaamse overheid
+
+
+## Installation
+```
+npm install --save vl-ui-template
+```
+
+## Demo
+```
+npm run demo
+```
+
+## Credits
+Zie de lijst van [ontwikkelaars](https://github.com/milieuinfo/webcomponent-vl-ui-template/graphs/contributors) die meegewerkt hebben aan de webcomponent.
+
+## Contact
+Heb je suggesties, opmerkingen of tips? Voel je dan vrij om ons te contacteren via help@omgevingvlaanderen.be.
---
+++ vl-template.src.js
@@ -0,0 +1,40 @@
+import {VlElement, define} from '/node_modules/vl-ui-core/vl-core.js';
+// import {VlHeader} from '/node_modules/vl-ui-header/vl-header.js';
+// import {VlFooter} from '/node_modules/vl-ui-footer/vl-footer.js';
+// todo add to package.json:
+//     "vl-ui-header": "1.0.0",
+//     "vl-ui-footer": "1.0.0"
+
+/**
+ * VlTemplate
+ * @class
+ * @classdesc De standaard template voor websites en applicaties van de Vlaamse overheid. <a href="demo/vl-template.html">Demo</a>.
+ *
+ * @extends VlElement
+ *
+ * @property {slot} header - header content binnen vl-header
+ * @property {slot} main - main content
+ * @property {slot} footer - footer content binnen vl-footer
+ */
+export class VlTemplate extends VlElement(HTMLElement) {
+  constructor() {
+    super(`
+            <style>
+                @import '../style.css';
+            </style>
+            <vl-header>
+              <slot name="header"></slot>
+            </vl-header>
+            <div class="vl-page">
+              <main class="vl-main-content">
+                <slot name="main"></slot>
+              </main>
+            </div>
+            <vl-footer>
+              <slot name="footer"></slot>
+            </vl-footer>
+        `);
+  }
+}
+
+define('vl-template', VlTemplate);
