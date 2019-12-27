8a06c0981eb244377302cc3dcc8da37d4f6e1355
Tom Coemans <tom.coemans@gmail.com>
2019-12-27 09:57:24 +0100

    build

--- vl-template.js
+++ vl-template.js
@@ -1,16 +1,12 @@
 import{VlElement,define}from"/node_modules/vl-ui-core/vl-core.js";export class VlTemplate extends VlElement(HTMLElement){constructor(){super(`
-            <style>
-                @import '/node_modules/vl-ui-template/style.css';
-            </style>
-            <vl-header>
-              <slot name="header"></slot>
-            </vl-header>
-            <div class="vl-page">
-              <main class="vl-main-content">
-                <slot name="main"></slot>
-              </main>
-            </div>
-            <vl-footer>
-              <slot name="footer"></slot>
-            </vl-footer>
-        `)}};define("vl-template",VlTemplate);
+        <style>
+            @import '/node_modules/vl-ui-template/style.css';
+        </style>
+        <vl-header>header</vl-header>
+        <div class="vl-page">
+          <main class="vl-main-content">
+            <slot></slot>
+          </main>
+        </div>
+        <vl-footer>footer</vl-footer>
+    `)}};define("vl-template",VlTemplate);
