c8f5c990e15fb6bc354dffd2a85b7db0375ca395
Tom Coemans <tom.coemans@gmail.com>
2020-04-17 09:44:18 +0200

    fixed demo code

--- dist/vl-template.js
+++ dist/vl-template.js
@@ -21,14 +21,14 @@ export class VlTemplate extends VlElement(HTMLElement) {
         <style>
             @import '/node_modules/vl-ui-template/dist/style.css';
         </style>
-        <div>
+        <div class="vl-u-sticky-gf">
           <slot name="header"></slot>
           <div class="vl-page">
             <main class="vl-main-content">
               <slot name="main"></slot>
             </main>
           </div>
-            <slot name="footer"></slot>
+          <slot name="footer"></slot>
         </div>
     `);
   }
