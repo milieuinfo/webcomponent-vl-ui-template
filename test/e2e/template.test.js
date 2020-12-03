d1816b9522d511b0107f53552cd754e9a9153421
coemans <tom.coemans@vlaanderen.be>
2020-12-03 16:42:21 +0100

    Upgrade huisstijl versie 3.12.3 (#84)

--- test/e2e/template.test.js
+++ test/e2e/template.test.js
@@ -9,22 +9,6 @@ describe('vl-template', async () => {
     return vlTemplatePage.load();
   });
 
-  it('als gebruiker kan ik de header zien', async () => {
-    const template = await vlTemplatePage.getTemplate();
-    const headerSlotElements = await template.getHeaderSlotElements();
-    assert.lengthOf(headerSlotElements, 1);
-    assert.isNotNull(headerSlotElements);
-    await assert.eventually.equal(headerSlotElements[0].getTagName(), 'vl-header');
-  });
-
-  it('als gebruiker kan ik de footer zien', async () => {
-    const template = await vlTemplatePage.getTemplate();
-    const footerSlotElements = await template.getFooterSlotElements();
-    assert.lengthOf(footerSlotElements, 1);
-    assert.isNotNull(footerSlotElements);
-    await assert.eventually.equal(footerSlotElements[0].getTagName(), 'vl-footer');
-  });
-
   it('als gebruiker kan ik de content zien', async () => {
     const template = await vlTemplatePage.getTemplate();
     const contentSlotElements = await template.getContentSlotElements();
